420de96c97e5aa0cf189d97e50083bd4665c7ad3
Laura <lmcsay@gmail.com>
2021-02-19 15:24:05 -0600

    Fix: Add ServerStyleSheet to fix style rendering issue

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,6 +1,18 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import Document, { Html, Head, NextScript, Main } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
+  // S
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage((App) => (props) =>
+      sheet.collectStyles(<App {...props} />)
+    );
+    const styleTags = sheet.getStyleElement();
+    return { ...page, styleTags };
+  }
+
   render() {
     return (
       <Html>

56ba0f9c30492deff882f76da91a424c75f05600
Laura <lmcsay@gmail.com>
2021-02-19 11:26:50 -0600

    Style: Add style to Nav component

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,11 +1,29 @@
 import Link from 'next/link';
+import styled from 'styled-components';
 import Nav from './Nav';
+
+const Logo = styled.h1`
+  font-size: 4rem;
+  margin-left: 2rem;
+  position: relative;
+  z-index: 2;
+  background: #20c4c4;
+  transform: skew(-7deg);
+  a {
+    color: white;
+    text-decoration: none;
+    text-transform: uppercase;
+    padding: 0.5rem 1rem;
+  }
+`;
 
 export default function Header() {
   return (
     <header>
       <div className="bar">
-        <Link href="/">Rad Merch</Link>
+        <Logo>
+          <Link href="/">Rad Merch</Link>
+        </Logo>
       </div>
       <div className="sub-bar">
         <p>Search</p>

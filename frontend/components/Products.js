ad12f304849c7e96694fe1a96ac02caeb3327588
Laura <lmcsay@gmail.com>
2021-02-20 13:36:23 -0600

    Feat: Add products component

---
+++ frontend/components/Products.js
@@ -0,0 +1,44 @@
+import { useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
+import styled from 'styled-components';
+import Product from './Product';
+
+const ALL_PRODUCTS_QUERY = gql`
+  query ALL_PRODUCTS_QUERY {
+    allProducts {
+      id
+      name
+      price
+      description
+      photo {
+        id
+        image {
+          publicUrlTransformed
+        }
+      }
+    }
+  }
+`;
+
+const ProductsListStyles = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 60px;
+`;
+
+export default function Products() {
+  // returns data, any errors and if it is currently loading
+  const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY);
+  console.log(data);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+  return (
+    <div>
+      <ProductsListStyles>
+        {data.allProducts.map((product) => (
+          <Product key={product.id} product={product} />
+        ))}
+      </ProductsListStyles>
+    </div>
+  );
+}

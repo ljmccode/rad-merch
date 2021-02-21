2f2b5f74184bb66d6ac0ee6bad45bc9f8391fb3f
Laura <lmcsay@gmail.com>
2021-02-20 18:53:19 -0600

    Feat: Add Image upload to sell form

--- frontend/lib/useForm.js
+++ frontend/lib/useForm.js
@@ -10,7 +10,7 @@ export default function useForm(initialState = {}) {
       value = parseInt(value);
     }
     if (type === 'file') {
-      value[0] = e.target.files;
+      [value] = e.target.files;
     }
     setInputs({
       // copy the existing state

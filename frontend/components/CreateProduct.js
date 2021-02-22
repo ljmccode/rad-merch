bce8938d1109396f57db01638dc49f39cfa6fea8
Laura <lmcsay@gmail.com>
2021-02-22 10:06:07 -0600

    Feat: Add GraphQL create product mutation

--- frontend/components/CreateProduct.js
+++ frontend/components/CreateProduct.js
@@ -1,5 +1,33 @@
+import { useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
 import useForm from '../lib/useForm';
 import Form from './styles/Form';
+
+const CREATE_PRODUCT_MUTATION = gql`
+  mutation CREATE_PRODUCT_MUTATION(
+    # which variables are getting passed in and what types
+    $name: String!
+    $description: String!
+    $price: Int!
+    $image: Upload
+  ) {
+    createProduct(
+      data: {
+        name: $name
+        description: $description
+        price: $price
+        status: "AVAILABLE"
+        # create a relationship and an item behind the scenes
+        photo: { create: { image: $image, altText: $name } }
+      }
+    ) {
+      id
+      price
+      description
+      name
+    }
+  }
+`;
 
 export default function CreateProduct() {
   const { inputs, handleChange } = useForm({
@@ -8,11 +36,22 @@ export default function CreateProduct() {
     price: 200000,
     description: 'Vintage jacket',
   });
+  // first thing it returns is the function that runs the mutation
+  const [createProduct, { data, error, loading }] = useMutation(
+    CREATE_PRODUCT_MUTATION,
+    {
+      variables: inputs,
+    }
+  );
+  console.log(createProduct);
   return (
     <Form
-      onSubmit={(e) => {
+      onSubmit={async (e) => {
         e.preventDefault();
         console.log(inputs);
+        // Submit the input fields to the backend
+        const res = await createProduct();
+        console.log(res);
       }}
     >
       <fieldset>

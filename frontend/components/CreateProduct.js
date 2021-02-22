b974c122a5c35ea27cf87f46a0bc18d18941de91
Laura <lmcsay@gmail.com>
2021-02-22 10:53:51 -0600

    Fix: Refetch query so new products shows w/o refresh

--- frontend/components/CreateProduct.js
+++ frontend/components/CreateProduct.js
@@ -3,10 +3,12 @@ import gql from 'graphql-tag';
 import useForm from '../lib/useForm';
 import Form from './styles/Form';
 import DisplayError from './ErrorMessage';
+import { ALL_PRODUCTS_QUERY } from './Products';
 
 const CREATE_PRODUCT_MUTATION = gql`
   mutation CREATE_PRODUCT_MUTATION(
     # which variables are getting passed in and what types
+    # ! means it is required
     $name: String!
     $description: String!
     $price: Int!
@@ -42,6 +44,7 @@ export default function CreateProduct() {
     CREATE_PRODUCT_MUTATION,
     {
       variables: inputs,
+      refetchQueries: [{ query: ALL_PRODUCTS_QUERY }]
     }
   );
   return (

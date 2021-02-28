2cd2edef8cd00bc51f61456191275844598fb7d1
Laura <lmcsay@gmail.com>
2021-02-27 19:15:35 -0600

    Feat: Get product to edit page from id

--- frontend/components/UpdateProduct.js
+++ frontend/components/UpdateProduct.js
@@ -1,3 +1,24 @@
-export default function UpdateProduct() {
-  return <p>Update</p>;
+import { useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
+
+const SINGLE_PRODUCT_QUERY = gql`
+  query SINGLE_PRODUCT_QUERY($id: ID!) {
+    Product(where: { id: $id }) {
+      id
+      name
+      description
+      price
+    }
+  }
+`;
+
+export default function UpdateProduct({ id }) {
+  const { data, error, loading } = useQuery(SINGLE_PRODUCT_QUERY, {
+    variables: { id },
+  });
+
+  console.log(data);
+
+  return <p>Update {id}</p>;
 }
+ 

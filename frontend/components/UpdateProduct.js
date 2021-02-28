8dcc4404ba066cb03db1be37600adb98c1141678
Laura <lmcsay@gmail.com>
2021-02-27 19:43:50 -0600

    Feat: Show product info on edit page form

--- frontend/components/UpdateProduct.js
+++ frontend/components/UpdateProduct.js
@@ -1,5 +1,8 @@
 import { useMutation, useQuery } from '@apollo/client';
 import gql from 'graphql-tag';
+import useForm from '../lib/useForm';
+import DisplayError from './ErrorMessage';
+import Form from './styles/Form';
 
 const SINGLE_PRODUCT_QUERY = gql`
   query SINGLE_PRODUCT_QUERY($id: ID!) {
@@ -20,13 +23,8 @@ const UPDATE_PRODUCT_MUTATION = gql`
     $price: Int
   ) {
     updateProduct(
-      id: $id,
-      data {
-        id: $id
-        name: $name
-        description: $description
-        price: $price 
-      }
+      id: $id
+      data: { id: $id, name: $name, description: $description, price: $price }
     ) {
       id
       name
@@ -34,7 +32,7 @@ const UPDATE_PRODUCT_MUTATION = gql`
       price
     }
   }
-  `;
+`;
 
 export default function UpdateProduct({ id }) {
   const { data, error, loading } = useQuery(SINGLE_PRODUCT_QUERY, {
@@ -51,5 +49,62 @@ export default function UpdateProduct({ id }) {
     },
   });
 
-  return <p>Update {id}</p>;
+  // Create some state for the form inputs
+  const { inputs, handleChange, clearForm } = useForm(data?.Product);
+  if (loading) return <p>Loading...</p>;
+
+  return (
+    <Form
+      onSubmit={async (e) => {
+        e.preventDefault();
+        // TODO: Handle Submit
+
+        // Submit the input fields to the backend
+        // const res = await createProduct();
+        // clearForm();
+        // // Go to that product's page
+        // Router.push({
+        //   pathname: `/product/${res.data.createProduct.id}`,
+        // });
+      }}
+    >
+      <DisplayError error={error || updateError} />
+      <fieldset disabled={updateLoading} aria-busy={loading}>
+        <label htmlFor="name">
+          Name
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Name"
+            value={inputs.name}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="price">
+          Price
+          <input
+            type="number"
+            id="price"
+            name="price"
+            placeholder="Price"
+            value={inputs.price}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="description">
+          Description
+          <textarea
+            id="description"
+            name="description"
+            placeholder="Description"
+            value={inputs.description}
+            onChange={handleChange}
+          />
+        </label>
+
+        <button type="submit">Update Product</button>
+      </fieldset>
+    </Form>
+  );
 }

850f9af27d0301cdf3994a649025c0a4a213eafe
Laura <lmcsay@gmail.com>
2022-10-21 13:59:48 -0500

    Create a cart contextto hold cart state

---
+++ frontend/lib/cartState.js
@@ -0,0 +1,41 @@
+import { createContext, useContext, useState } from 'react';
+import PropTypes from 'prop-types';
+
+const LocalStateContext = createContext();
+const LocalStateProvider = LocalStateContext.Provider;
+
+function CartStateProvider({ children }) {
+  const [cartOpen, setCartOpen] = useState(false);
+
+  function toggleCart() {
+    setCartOpen(!cartOpen);
+  }
+
+  function closeCart() {
+    setCartOpen(false);
+  }
+
+  function openCart() {
+    setCartOpen(true);
+  }
+
+  return (
+    <LocalStateProvider
+      value={{ cartOpen, setCartOpen, toggleCart, closeCart, openCart }}
+    >
+      {children}
+    </LocalStateProvider>
+  );
+}
+
+// Custom hook for accessing the cart local state
+function useCart() {
+  const all = useContext(LocalStateContext);
+  return all;
+}
+
+export { CartStateProvider, useCart };
+
+CartStateProvider.propTypes = {
+  children: PropTypes.any,
+};

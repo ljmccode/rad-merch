8d61745c0a5d4cefd2c4c1d8e97cfeace1437344
Laura <lmcsay@gmail.com>
2022-10-20 19:48:14 -0500

    Add SignIn and SignUp components to sign in page

--- frontend/pages/signin.js
+++ frontend/pages/signin.js
@@ -1,5 +1,18 @@
-const SignInPage = () => {
-  return <div>Sign In</div>;
-};
+import styled from 'styled-components';
+import SignIn from '../components/SignIn';
+import SignUp from '../components/SignUp';
+
+const GridStyles = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-gap: 2rem;
+`;
+
+const SignInPage = () => (
+  <GridStyles>
+    <SignIn />
+    <SignUp />
+  </GridStyles>
+);
 
 export default SignInPage;

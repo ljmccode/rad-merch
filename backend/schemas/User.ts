4f2f1f182abffd480711c56990b1e5a61304537c
Laura <lmcsay@gmail.com>
2022-10-24 17:33:38 -0500

    Create a relationship between user and products

--- backend/schemas/User.ts
+++ backend/schemas/User.ts
@@ -25,5 +25,9 @@ export const User = list({
       ref: 'Role.assignedTo',
       // TODO: Add access control
     }),
+    products: relationship({
+      ref: 'Product.user',
+      many: true,
+    }),
   },
 });
